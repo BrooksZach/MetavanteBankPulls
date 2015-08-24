06cd9c1e0e5eae96fbda11f77756558ece63d442
BrooksZach <zachary.brooks@lenderlive.com>
2015-08-24 08:20:05 -0600

    Initial Commit

---
+++ MetavanteWebScraper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetavanteWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
